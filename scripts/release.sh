8649624e99ddd6ab548a48efa14e37e2d6e864d0
Vlad Emelianov <volshebnyi@gmail.com>
2019-11-09 00:18:45 +0100

    Bump version to 0.1.2

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,13 +6,12 @@ OUTPUT_PATH=${ROOT_PATH}/mypy_boto3_output
 
 release_package() {
     cd $1
-    rm -rf build *.egg-info dist/*
-    python setup.py sdist bdist_wheel 2>&1
-
-    pipenv run twine upload dist/*
-
-    rm -rf build *.egg-info dist/*
+    echo $1
+    rm -rf build *.egg-info dist/* 2>&1 > /dev/null
+    python setup.py sdist bdist_wheel 2>&1 > /dev/null
+    pipenv run twine upload dist/* 2>&1 > /dev/null
+    rm -rf build *.egg-info dist/* 2>&1 > /dev/null
 }
 export -f release_package
 
-find ${OUTPUT_PATH} -mindepth 1 -maxdepth 1 -name 'mypy_boto3_*' -type d -exec bash -c 'release_package "$@"' bash {} \;
+find ${OUTPUT_PATH} -mindepth 1 -maxdepth 1 -name 'mypy_boto3_*' -type d | xargs -n 10 bash -c 'release_package "$@"'
