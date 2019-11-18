de95ddfd3636cf415cd819f1f9cd00b20fca4db0
Vlad Emelianov <volshebnyi@gmail.com>
2019-11-18 21:55:18 +0100

    Build packages on release

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,4 @@ set -e
 ROOT_PATH=$(dirname $(dirname $(realpath $0)))
 cd ${ROOT_PATH}
 
-mypy_boto3_builder mypy_boto3_output --skip-master --skip-stubs $@
-mypy_boto3_builder mypy_boto3_output --skip-services
+mypy_boto3_builder mypy_boto3_output $@
