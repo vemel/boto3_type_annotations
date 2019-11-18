91dcb623d71eb6de4b61339ecf2a4b3b4bd10ffd
Vlad Emelianov <volshebnyi@gmail.com>
2019-11-18 04:50:10 +0100

    Do not report errors on upload

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,7 +14,7 @@ do
     cd ${package}
     rm -rf build *.egg-info dist/* > /dev/null
     python setup.py build sdist bdist_wheel 1>/dev/null 2>/dev/null
-    twine upload dist/* > /dev/null
+    twine upload dist/* > /dev/null || true
 done
 
 cd ${OUTPUT_PATH}/master_package
