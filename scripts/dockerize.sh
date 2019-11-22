7b829dfd6df299047bec4ea750ca0fef12e1bd27
Vlad Emelianov <volshebnyi@gmail.com>
2019-11-22 10:06:15 +0100

    Reorder commands in dockerize step

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -10,7 +10,7 @@ if [[ "$VERSION" == "" ]]; then
 fi
 
 echo "Dockerizing mypy_boto3_builder ${VERSION}"
-docker login docker.pkg.github.com --username vemel -p ${GITHUB_TOKEN}
 docker build . --tag mypy_boto3_builder
 docker tag mypy_boto3_builder docker.pkg.github.com/vemel/mypy_boto3/mypy_boto3_builder:${VERSION}
+docker login docker.pkg.github.com --username vemel -p ${GITHUB_TOKEN}
 docker push docker.pkg.github.com/vemel/mypy_boto3/mypy_boto3_builder:${VERSION}
