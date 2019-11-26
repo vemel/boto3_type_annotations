d30f39019daa8e5012c54c059ec80bb0e11b1de9
Vlad Emelianov <volshebnyi@gmail.com>
2019-11-26 01:33:41 +0100

    Announce updates to service modules

--- builder/mypy_boto3_builder/writers/processors.py
+++ builder/mypy_boto3_builder/writers/processors.py
@@ -6,6 +6,7 @@ from typing import Iterable
 
 from boto3.session import Session
 
+from mypy_boto3_builder.enums.service_module_name import ServiceModuleName
 from mypy_boto3_builder.version import __version__ as version
 from mypy_boto3_builder.parsers.master_package import parse_master_package
 from mypy_boto3_builder.parsers.boto3_stubs_package import parse_boto3_stubs_package
@@ -60,7 +61,11 @@ def process_service(
     logger.debug(f"Writing {service_name.boto3_name} to {NicePath(output_path)}")
 
     modified_paths = write_service_package(service_module, output_path)
+    module_file_names = [i.value for i in ServiceModuleName]
     for modified_path in modified_paths:
-        logger.debug(f"Updated {NicePath(modified_path)}")
+        if modified_path.stem in module_file_names:
+            logger.info(f"Updated {NicePath(modified_path)}")
+        else:
+            logger.debug(f"Updated {NicePath(modified_path)}")
 
     return service_module
