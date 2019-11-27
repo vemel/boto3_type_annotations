158c8f0302057efe105be4b46f3127303567f369
Vlad Emelianov <volshebnyi@gmail.com>
2019-11-27 18:46:18 +0100

    Ignore missing modules in typing for pylint

--- builder/mypy_boto3_builder/mypy_boto3_static/type_defs.py
+++ builder/mypy_boto3_builder/mypy_boto3_static/type_defs.py
@@ -5,7 +5,7 @@ for undocumented methods.
 import sys
 
 if sys.version_info >= (3, 8):
-    from typing import TypedDict, Literal, overload
+    from typing import TypedDict, Literal, overload  # pylint: disable=no-name-in-module
 else:
     from typing_extensions import TypedDict, Literal, overload
 
--- builder/mypy_boto3_builder/type_defs.py
+++ builder/mypy_boto3_builder/type_defs.py
@@ -4,7 +4,7 @@ Provides compatibility between `typing` and `typing_extensions`.
 import sys
 
 if sys.version_info >= (3, 8):
-    from typing import TypedDict, Literal, overload
+    from typing import TypedDict, Literal, overload  # pylint: disable=no-name-in-module
 else:
     from typing_extensions import TypedDict, Literal, overload
 
