166f0ddd898b9c7300ebe34f93e413d7f7b85cdd
Vlad Emelianov <volshebnyi@gmail.com>
2019-11-27 01:30:34 +0100

    Add integration tests

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,17 @@ if [[ "$1" == "" ]]; then
     PACKAGES=${OUTPUT_PATH}/mypy_boto3_*
 fi
 
+if [[ "$1" == "master" ]]; then
+    echo Installing master package
+    cd ${OUTPUT_PATH}/master_package
+    python setup.py install 1>/dev/null 2>/dev/null
+
+    echo Installing boto3-stubs package
+    cd ${OUTPUT_PATH}/boto3_stubs_package
+    python setup.py install 1>/dev/null 2>/dev/null
+    exit
+fi
+
 for package in $PACKAGES
 do
     echo Installing $(basename ${package})
@@ -15,10 +26,3 @@ do
     python setup.py install 1>/dev/null 2>/dev/null
 done
 
-echo Installing master package
-cd ${OUTPUT_PATH}/master_package
-python setup.py install 1>/dev/null 2>/dev/null
-
-echo Installing boto3-stubs package
-cd ${OUTPUT_PATH}/boto3_stubs_package
-python setup.py install 1>/dev/null 2>/dev/null
---
+++ scripts/integration.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+ROOT_PATH=$(dirname $(dirname $(realpath $0)))
+
+EXAMPLES=${ROOT_PATH}/examples/*_example.py
+
+${ROOT_PATH}/scripts/install.sh master
+
+for EXAMPLE in $EXAMPLES
+do
+    FILE_NAME=$(basename ${EXAMPLE})
+    SERVICE_NAME=`echo ${FILE_NAME} | sed 's/_example\.py//'`
+    DIR_NAME=$(dirname ${EXAMPLE})
+    EXPECTED=${DIR_NAME}/e2e_snapshots/${FILE_NAME}.out
+    OUTPUT=${DIR_NAME}/e2e_snapshots/current.out
+
+    ${ROOT_PATH}/scripts/install.sh ${SERVICE_NAME}
+    echo Running mypy for ${FILE_NAME}
+    if [[ ! -f ${EXPECTED} ]]; then
+        mypy ${EXAMPLE} 1> ${EXPECTED} || true
+        echo "Created ${EXPECTED}"
+        continue
+    fi
+    mypy ${EXAMPLE} 1> ${OUTPUT} || true
+    DIFF=`diff ${OUTPUT} ${EXPECTED}` || true
+    rm ${OUTPUT}
+
+    if [[ ${DIFF} != "" ]]; then
+        echo Output for "mypy ${FILE_NAME}" is different:
+        echo $DIFF
+        echo
+        echo "If it is expected, run: rm ${EXPECTED}"
+        exit 1
+    fi
+done
