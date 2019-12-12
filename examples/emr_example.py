21ada35f772936144241625f93c2c93e35308788
Vlad Emelianov <volshebnyi@gmail.com>
2019-12-12 09:06:57 +0100

    FIx tests

--- examples/emr_example.py
+++ examples/emr_example.py
@@ -5,7 +5,6 @@ from mypy_boto3.emr import Client
 
 
 def emr_client_example() -> None:
-    # equivalent of `boto3.client('s3')`
     client: Client = boto3.client("emr")
     client.cancel_steps("cluster_id", [123])
 
