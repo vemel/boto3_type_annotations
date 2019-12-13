e4ad9a1cedaaa4bcb282185af40e6d9f1dd6fb0d
Vlad Emelianov <volshebnyi@gmail.com>
2019-12-13 02:47:07 +0100

    Update dynamodb test

--- examples/dynamodb_example.py
+++ examples/dynamodb_example.py
@@ -1,11 +1,17 @@
 # install `pip install boto3-stubs[dynamodb]`
 
 import boto3
-from mypy_boto3.dynamodb import Client
+from mypy_boto3.dynamodb import DynamoDBClient, DynamoDBServiceResource
+from mypy_boto3.dynamodb.service_resource import Table
 
 
 def dynamodb_client_example() -> None:
-    client: Client = boto3.client("dynamodb")
+    client: DynamoDBClient = boto3.client("dynamodb")
+    resource: DynamoDBServiceResource = boto3.client("dynamodb")
+
+    my_table: Table = resource.Table("my_table")
+    print(my_table.name)
+
     try:
         client.list_backups(TableName=123)
     except client.exceptions.BackupInUseException as e:
