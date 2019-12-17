4b55d387b482f5d306a3908e0ebc1c4a41d0e701
Vlad Emelianov <volshebnyi@gmail.com>
2019-12-17 22:16:18 +0100

    Add vulture to before_commit

--- scripts/before_commit.sh
+++ scripts/before_commit.sh
@@ -4,6 +4,7 @@ set -e
 ROOT_PATH=$(dirname $(dirname $(realpath $0)))
 cd $ROOT_PATH
 
+vulture builder/mypy_boto3_builder vulture_whitelist.txt
 mypy builder/mypy_boto3_builder
 pylint builder/mypy_boto3_builder
 pytest
