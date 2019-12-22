29d9ac8a656f6da7b9462281d4cdf091ed412741
Vlad Emelianov <volshebnyi@gmail.com>
2019-12-22 18:51:46 +0100

    Extract version from file

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -4,11 +4,7 @@ set -e
 ROOT_PATH=$(dirname $(dirname $(realpath $0)))
 cd ${ROOT_PATH}
 
-if [[ "$VERSION" == "" ]]; then
-    echo "No VERSION specified"
-    exit 1
-fi
-
+VERSION=`cat builder/mypy_boto3_builder/version.py | grep -Po '\d+(\.\d+)+'`
 echo "Dockerizing mypy_boto3_builder ${VERSION}"
 docker build . --tag mypy_boto3_builder
 docker tag mypy_boto3_builder docker.pkg.github.com/vemel/mypy_boto3/mypy_boto3_builder:${VERSION}
