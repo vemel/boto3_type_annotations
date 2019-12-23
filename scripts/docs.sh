d6c07fe89b75a91829f44894d186c5b3ce8900eb
Vlad Emelianov <volshebnyi@gmail.com>
2019-12-23 20:17:27 +0100

    Remove build docs

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -4,4 +4,4 @@ set -e
 ROOT_PATH=$(dirname $(dirname $(realpath $0)))
 cd $ROOT_PATH
 
-handsdown --external `git config --get remote.origin.url` -n mypy-boto3 builder --cleanup --panic
+handsdown --external `git config --get remote.origin.url` -n mypy-boto3 builder --exclude '*/build/*' --cleanup --panic
