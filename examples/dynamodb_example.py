4fc3e7b7aa881fe935cff57fd807bd30a7ae0f8b
Vlad Emelianov <volshebnyi@gmail.com>
2020-01-08 23:48:04 +0100

    Update integration tests

--- examples/dynamodb_example.py
+++ examples/dynamodb_example.py
@@ -8,7 +8,7 @@ from mypy_boto3.dynamodb.service_resource import Table
 
 def dynamodb_client_example() -> None:
     client: DynamoDBClient = boto3.client("dynamodb")
-    resource: DynamoDBServiceResource = boto3.client("dynamodb")
+    resource: DynamoDBServiceResource = boto3.resource("dynamodb")
 
     my_table: Table = resource.Table("my_table")
     print(my_table.name)
